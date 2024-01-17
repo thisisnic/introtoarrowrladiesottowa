53efffabb6d414d641fe0bd3d03e8a73de043e5d
Nic Crane <thisisnic@gmail.com>
2024-01-17 13:49:14 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Arrow 2 Hour Workshop
 
+TODO:
+* Once finalised content, run and then delete script_to_remove_unusued_images.R
---
+++ script_to_remove_unused_images.R
@@ -0,0 +1,11 @@
+lines <- readLines("slides.qmd")
+images <- lines[stringr::str_detect(lines, "images/")]
+used_images <- images %>%
+  stringr::str_extract("images/.*?[\\)|\"]") %>%
+  stringr::str_remove("[\\)|\"]$")
+
+in_dir <- fs::dir_ls("images/", recurse = TRUE)
+
+setdiff(in_dir, used_images) %>%
+  fs::file_delete()
+
